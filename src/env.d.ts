b5a8eb096bed5130a3a68a9469990cd94fd048e9
Gustavo <gustavo@obex.pw>
2022-11-03 13:12:34 -0300

    changed create-react-app to vite

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly REACT_APP_CAPTCHA_SITE_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import nodePolyfills from "vite-plugin-node-stdlib-browser";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [nodePolyfills(), react()],
+  build: {
+    outDir: "build",
+  },
+  server: {
+    open: true,
+  },
+  define: {
+    global: "globalThis",
+  },
+});
