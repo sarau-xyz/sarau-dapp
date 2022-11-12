60df6441056f87649e26570070a0acaeed33c779
Gustavo <gustavo@obex.pw>
2022-11-12 17:04:35 -0300

    added new addresses

--- src/constants/SARAU_MAKER_ADDRESSES.ts
+++ src/constants/SARAU_MAKER_ADDRESSES.ts
@@ -1,4 +1,5 @@
 export const SARAU_MAKER_ADDRESSES: { [chainId: string]: string } = {
-  "44787": "0x018449F1f4a06BC4E1Cb4De53FFB382664477c78", // alfajores
+  "31337": "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // hardhat
+  "44787": "0xee06133f734895a96a2d3D1f05905B570D978B05", // alfajores
   "42220": "", // celo
 };
