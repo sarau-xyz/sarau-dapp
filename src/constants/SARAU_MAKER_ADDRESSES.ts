2eaf281e6cd87bd41873736a3c52d1ea0d871651
Gustavo <gustavo@obex.pw>
2022-11-12 20:17:27 -0300

    added celo contract

--- src/constants/SARAU_MAKER_ADDRESSES.ts
+++ src/constants/SARAU_MAKER_ADDRESSES.ts
@@ -3,5 +3,5 @@ import { CUSTOM_CHAINS } from "./CUSTOM_CHAINS";
 export const SARAU_MAKER_ADDRESSES: { [chainId: string]: string } = {
   // [CUSTOM_CHAINS.hardhat.id]: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512", // hardhat
   [CUSTOM_CHAINS.alfajores.id]: "0x00AC0451422cb0C85D9D6523D96563509Dbe7674", // alfajores
-  [CUSTOM_CHAINS.celo.id]: "", // celo
+  [CUSTOM_CHAINS.celo.id]: "0x27b6B90B6519F3b3B224301366be3a10A203997f", // celo
 };
