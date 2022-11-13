785ce1d99c3511b84b585ec2d7987cf3f998529b
Gustavo <gustavo@obex.pw>
2022-11-13 04:19:14 -0300

    fix network ordering

--- src/constants/CUSTOM_CHAINS.ts
+++ src/constants/CUSTOM_CHAINS.ts
@@ -19,24 +19,6 @@ export const CUSTOM_CHAINS: {
   //   },
   //   testnet: true,
   // },
-  alfajores: {
-    id: 44787,
-    name: "Celo (Alfajores Testnet)",
-    network: "alfajores",
-    nativeCurrency: {
-      decimals: 18,
-      name: "Celo",
-      symbol: "CELO",
-    },
-    rpcUrls: {
-      default: "https://celo-hackathon.lavanet.xyz/celo-alfajores/http",
-      public: "https://alfajores-forno.celo-testnet.org/",
-    },
-    blockExplorers: {
-      default: { name: "CeloScan", url: "https://alfajores.celoscan.io/" },
-    },
-    testnet: true,
-  },
   celo: {
     id: 42220,
     name: "Celo (Mainnet)",
@@ -54,6 +36,24 @@ export const CUSTOM_CHAINS: {
       default: { name: "CeloScan", url: "https://celoscan.io/" },
     },
     testnet: false,
+  },
+  alfajores: {
+    id: 44787,
+    name: "Celo (Alfajores Testnet)",
+    network: "alfajores",
+    nativeCurrency: {
+      decimals: 18,
+      name: "Celo",
+      symbol: "CELO",
+    },
+    rpcUrls: {
+      default: "https://celo-hackathon.lavanet.xyz/celo-alfajores/http",
+      public: "https://alfajores-forno.celo-testnet.org/",
+    },
+    blockExplorers: {
+      default: { name: "CeloScan", url: "https://alfajores.celoscan.io/" },
+    },
+    testnet: true,
   },
 };
 
